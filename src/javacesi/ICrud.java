13ac5e13438648a15f9d8b3d22350702a3d4ec55
Amanite <amanitep@gmail.com>
2016-10-03 11:50:00 +0200

    interface crud

--- src/javacesi/ICrud.java
+++ src/javacesi/ICrud.java
@@ -4,4 +4,9 @@ package javacesi;
  * Created by Amanite on 03/10/2016.
  */
 public interface ICrud {
+
+    void Create(int id);
+    <T> void Read(T object);
+    <T> void Update(T object);
+    void Delete(int id);
 }
