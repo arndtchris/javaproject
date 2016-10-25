898e6b3ba077cff0e3f2d2dbd43fe2e2aa68c0e1
chrisarndt <arndt_c@ymail.com>
2016-10-25 11:04:19 +0200

    Mise en forme du document Cataloguefrais2016.xml et Récupération de la liste des frais dans un servlet

--- src/Controllers/FraisController.java
+++ src/Controllers/FraisController.java
@@ -1,11 +1,15 @@
 package Controllers;
 
+import javacesi.OperationXML;
+import javacesi.Parse;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by chris on 25/10/2016.
@@ -18,6 +22,9 @@ public class FraisController extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        ArrayList<OperationXML> lesOperations = new Parse().parseOperation();
+
         request.getRequestDispatcher("/frais.jsp").forward(request, response);
     }
 }
