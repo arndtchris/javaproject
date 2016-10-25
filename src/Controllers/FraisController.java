ea63db7b4776bda4961d222bd668390f86a0f485
chrisarndt <arndt_c@ymail.com>
2016-10-25 09:47:32 +0200

    Définition d'un controller destiné à la gestion des frais

---
+++ src/Controllers/FraisController.java
@@ -0,0 +1,23 @@
+package Controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by chris on 25/10/2016.
+ */
+@WebServlet(name = "FraisController")
+public class FraisController extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/frais.jsp").forward(request, response);
+    }
+}
